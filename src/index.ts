e387ab7cf79039ef77ff72519bae3d0b00118f04
Salma Ahmed <108466703+Salmaahmed8@users.noreply.github.com>
2025-02-04 17:14:05 +0200

    feat(tasks): Implement CRUD operations with soft delete & pagination

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,8 @@ import dotenv from "dotenv";
 import cors from "cors";
 import connectDB from "./config/db";
 import authRoutes from "./routes/authRoutes";
+import taskRoutes from "./routes/taskRoutes";
+
 import "express-async-errors";
 
 dotenv.config();
@@ -13,6 +15,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
+app.use("/api/tasks", taskRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
